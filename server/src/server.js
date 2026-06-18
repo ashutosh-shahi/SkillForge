37853deead4f38079e7918a55ac030bb42248866
Ashutosh Shahi <shahiashu.india@gmail.com>
2026-06-18 19:34:48 +0530

    Configure production deployment

--- server/src/server.js
+++ server/src/server.js
@@ -17,7 +17,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://skill-forge-two-beta.vercel.app"],
     credentials: true,
   })
 );
